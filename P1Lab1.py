d24d628f285740f184b6f70caf47e3f4c218440b
KeoniDecipulo <166351717+KeoniDecipulo@users.noreply.github.com>
2024-04-07 22:45:15 -0400

    Add files via upload

---
+++ P1Lab1.py
@@ -0,0 +1,7 @@
+#Keoni Decipulo
+#4/7/24
+#P1LAB1
+#2 variables with input functions to finish printing out one message.
+FirstName=input('Enter your first name:')
+LastName=input('Enter you last name:')
+print('Hello,', FirstName, LastName, end='!' ' Welcome to CTI-110!')
